9e8ac3b28e10766a530de09e0414c577546a0e58
JiKeZhang <3136713504@qq.com>
2022-03-27 18:10:44 +0800

    test commit

--- src/tx50/LC236.java
+++ src/tx50/LC236.java
@@ -16,6 +16,7 @@ public class LC236 {
         if(root == null || root == p || root == q) return root;
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left == null || right == null) return null;
         if(left == null) return right;
         if(right == null) return left;
         return root;
