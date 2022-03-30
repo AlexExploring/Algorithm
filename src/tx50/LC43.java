816c2e5f05f890640efa21bdf1f5f551e9109fe4
JiKeZhang <3136713504@qq.com>
2022-03-30 18:39:31 +0800

    test commit

--- src/tx50/LC43.java
+++ src/tx50/LC43.java
@@ -23,9 +23,9 @@ public class LC43 {
             int carry = 0;
 
             for (int j = num1.length()-1; j >= 0 || carry != 0; j--) {
-                int n1 = j < 0 ? 0 : num1.charAt(j);
-                int pro = (n1*n2+carry)%10;
-                temp.append(pro);
+                int n1 = j < 0 ? 0 : num1.charAt(j)-'0';
+                int cur = (n1*n2+carry)%10;
+                temp.append(cur);
                 carry = (n1*n2+carry)/10;
             }
             res = addStrings(res,temp.reverse().toString());
@@ -44,6 +44,31 @@ public class LC43 {
         }
         return res.reverse().toString();
     }
+
+    public String multiply1(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) return "0";
+        int [] res = new int[num1.length()+num2.length()];
+        for (int i = num1.length()-1; i >= 0 ; i--) {
+            int n1 = num1.charAt(i)-'0';
+            for (int j = num2.length()-1; j >= 0 ; j--) {
+                int n2 = num2.charAt(j)-'0';
+                //res[i+j+1 考虑之前的计算产生的进位
+                int sum = (res[i+j+1]+n1*n2);
+                res[i+j+1] = sum%10;
+                res[i+j] += sum/10;
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < res.length; i++) {
+            //去除前导0
+            if (i == 0 && res[i] == 0) continue;
+            result.append(res[i]);
+        }
+
+        return result.toString();
+    }
+
 }
 
 
