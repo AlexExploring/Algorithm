9d87820854bfe4f9ad0226f75c3e7528395208a2
JiKeZhang <3136713504@qq.com>
2022-04-02 23:29:09 +0800

    test commit

--- src/JZOFII/JZ119.java
+++ src/JZOFII/JZ119.java
@@ -1,6 +1,8 @@
 package JZOFII;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 最长连续序列
@@ -13,15 +15,40 @@ public class JZ119 {
         if (len < 2) return len;
 
         Arrays.sort(nums);
-        int cur = 1,ans = 0;
+        int cur = 1,ans = 1;
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i]-1 != nums[i-1]) {
+            if (nums[i] == nums[i-1]) {
+                continue;
+            }else if (nums[i]-1 == nums[i-1]) {
+                cur++;
+            }else {
                 ans = Math.max(cur,ans);
                 cur = 1;
-            }else {
-                cur++;
             }
         }
+
+        return Math.max(cur,ans);
+    }
+
+    public int longestConsecutive1(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+
+        int ans = 0;
+        for (Integer num : numSet) {
+            if (!numSet.contains(num-1)) {
+                int curNum = num;
+                int curAns = 1;
+                while (numSet.contains(curNum+1)) {
+                    curNum += 1;
+                    curAns += 1;
+                }
+                ans = Math.max(ans,curAns);
+            }
+        }
+
         return ans;
     }
 }
