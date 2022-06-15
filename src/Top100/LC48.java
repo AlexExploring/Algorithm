fad75fff56fb9ad178734e1dd7566c8ae1475e7b
zhxxltx <3136713504@qq.com>
2022-06-15 09:33:58 +0800

    test

--- src/Top100/LC48.java
+++ src/Top100/LC48.java
@@ -1,5 +1,7 @@
 package Top100;
 
+import backTrack.arrangement.LC47I;
+
 /**
  * 旋转图像
  *
@@ -7,19 +9,28 @@ package Top100;
  */
 public class LC48 {
 
+    public static void main(String[] args) {
+        int [][] mat = new int[3][];
+        mat[0] = new int[]{1,2,3};
+        mat[1] = new int[]{4,5,6};
+        mat[2] = new int[]{7,8,9};
+        new LC48().rotate(mat);
+    }
+
     /**
      * 方法一
+     *
+     * 内层循环一次确定四个数的位置
      */
-    public static void rotate(int[][] matrix) {
-        int len = matrix.length;
-
-        for (int t1 = 0,t2 = len-(t1+1); t1 < len/2; t1++,t2--) {
-            for (int i1 = t1,i2 = len-(t1+1),j1 = t1,j2 = len-(t1+1);
-                 i1 < len-(t1+1) && i2 > t1 && j1 < len-(t1+1) && j2 > t1;
-                    i1++,i2--,j1++,j2--) {
-                int a = matrix[t1][j1],b=matrix[i1][t2],
-                        c=matrix[t2][j2],d=matrix[i2][t1];
-                matrix[t1][j1]=d;matrix[i1][t2]=a;matrix[t2][j2]=b;matrix[i2][t1]=c;
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < (n + 1) / 2; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = temp;
             }
         }
     }
@@ -29,7 +40,7 @@ public class LC48 {
      *
      * 一张图先上下反转，再对角反转（右上半角和左下半角交换）
      */
-    public static void rotate1(int[][] matrix) {
+    public void rotate1(int[][] matrix) {
         int len = matrix.length;
         //上下反转
         for (int i = 0; i < len/2; i++) {
